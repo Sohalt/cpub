f7b87665fd76d14d181fe974b88b722ecd3b3249
sohalt <sohalt@sohalt.net>
2021-09-17 19:52:32 +0200

    devshell

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
+    #nixpkgs.url = "nixpkgs/nixos-21.05";
     utils.url = "github:numtide/flake-utils";
   };
 
@@ -20,28 +21,28 @@
 
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-    in
-    {
-      overlay = final: prev: {
-        cpub = with final; let
-          mixNixDeps = import ./deps.nix {
+
+      mixNixDeps' = pkgs: with pkgs; import ./deps.nix {
             inherit beamPackages lib;
             overrides = (final: prev: {
               mime = prev.mime.override {
-                patchPhase = let
-                  cfgFile = writeText "config.exs" ''
-                    use Mix.Config
-                    config :mime, :types, %{
-                      "application/ld+json" => ["jsonld"],
-                      "application/rdf+json" => ["rj"],
-                      "text/turtle" => ["ttl"]
-                    }
-                  '';
-                  in ''
+                patchPhase =
+                  let
+                    cfgFile = writeText "config.exs" ''
+                      use Mix.Config
+                      config :mime, :types, %{
+                        "application/ld+json" => ["jsonld"],
+                        "application/rdf+json" => ["rj"],
+                        "text/turtle" => ["ttl"]
+                      }
+                    '';
+                  in
+                  ''
                     mkdir config
                     cp ${cfgFile} config/config.exs
                   '';
               };
+              # mix2nix currently cannot deal with git dependencies, so they are added here
               eris = beamPackages.buildMix rec {
                 name = "eris";
                 version = "main";
@@ -71,74 +72,86 @@
                   rev = "857079b256303b5ae9efef795f6b043e80c3b837";
                   sha256 = "FzDQwvOdCudvvvk5oBXLNfL+1gAiHFgHcAuufRteGzU=";
                 };
-                beamDeps = with final; [decimal protocol_ex];
+                beamDeps = with final; [ decimal protocol_ex ];
               };
             });
           };
+    in
+    {
+      overlay = final: prev: {
+        cpub = with final; let
+          mixNixDeps = mixNixDeps' final;
         in
-        beamPackages.mixRelease rec {
-          inherit mixNixDeps;
-          pname = "cpub";
-          src = ./.;
-          version = "0.2.0"; # FIXME
-          preConfigure = ''
-            touch config/prod.secret.exs
-          '';
-          postInstall = ''
-            echo "${beamPackages.erlang.version}" > $out/OTP_VERSION
-          '';
-        };
+          beamPackages.mixRelease {
+            inherit mixNixDeps;
+            pname = "cpub";
+            src = ./.;
+            version = "0.2.0"; # FIXME
+            preConfigure = ''
+              touch config/prod.secret.exs
+            '';
+            postInstall = ''
+              echo "${beamPackages.erlang.version}" > $out/OTP_VERSION
+            '';
+          };
       };
       packages = forAllSystems (system:
         {
           inherit (nixpkgsFor.${system}) cpub;
         });
       defaultPackage = forAllSystems (system: self.packages.${system}.cpub);
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor."${system}";
+        in
+        pkgs.mkShell {
+          inputsFrom = builtins.attrValues self.packages.${system};
+        });
       nixosModules.cpub = { config, lib, ... }:
         with lib;
         let cfg = config.services.cpub;
         in
         {
-        options = {
-          services.cpub = {
-            enable = mkEnableOption "Cpub, a semantic ActivityPub server";
-            domain = mkOption {
-              type = types.str;
-              default = "localhost";
+          options = {
+            services.cpub = {
+              enable = mkEnableOption "Cpub, a semantic ActivityPub server";
+              domain = mkOption {
+                type = types.str;
+                default = "localhost";
+              };
+              port = mkOption {
+                type = types.port;
+                default = 4000;
+              };
+              databaseDir = mkOption {
+                type = str;
+                default = "/var/lib/cpub";
+              };
+              #secrets
+              cookieFile = mkOption {
+                type = str;
+              };
+              secretKeyBaseFile = mkOption {
+                type = str;
+              };
+              jokenRSAKey = mkOption {
+                type = str;
+              };
             };
-            port = mkOption {
-              type = types.port;
-              default = 4000;
+          };
+          config = mkIf cfg.enable {
+            systemd.services.cpub = {
+              description = "";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+
+              environment.COOKIE = "<(${cookieFile})";
+
+              serviceConfig = {
+                ExecStart = "${pkgs.cpub}/bin/cpub start";
+              };
             };
-	    databaseDir = mkOption {
-	      type = str;
-	      default = "/var/lib/cpub";
-            };
-	    #secrets
-	    cookieFile = mkOption {
-	      type = str;
-	    };
-	    secretKeyBaseFile = mkOption {
-	      type = str;
-	    };
-	    jokenRSAKey = mkOption {
-	      type = str;
-	    };
           };
         };
-        config = mkIf cfg.enable {
-          systemd.services.cpub = {
-	    description = "";
-	    after = [ "network.target" ];
-	    wantedBy = [ "multi-user.target" ];
-
-	    environment.COOKIE = "<(${cookieFile})"
-	    
-	    serviceConfig = {
-	      ExecStart = "${pkgs.cpub}/bin/cpub start";
-	    };
-	  };
-        };
-      };
     };
 }
